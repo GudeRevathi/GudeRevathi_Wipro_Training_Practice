db4697f8bff523d3651a844a6e5297312ea14f96
GudeRevathi <revathigude2906@gmail.com>
2025-08-11 19:14:56 +0530

    Day 1 - 19 July 2025 projects

---
+++ Arithematicoperations.java
@@ -0,0 +1,13 @@
+public class Arithematicoperations {
+    public static void main(String[] args){
+        int n1=Integer.parseInt(args[0]);
+        int n2=Integer.parseInt(args[1]);
+
+        System.out.println("addition: "+(n1+n2));
+        System.out.println("Subtraction: "+(n1-n2));
+        System.out.println("Multiplication: "+(n1*n2));
+        System.out.println("Division is: "+(n1/n2));
+
+
+    }
+}
---
+++ Welcome.java
@@ -0,0 +1,18 @@
+public class Welcome {
+    public static void main(String[] args){
+//        System.out.println("Hello World");
+//
+//        int x,y;
+//        x=y=10;
+//        System.out.println(x+y);
+//        System.out.println(10+20);
+//        float n=11.3f;
+//        int num=(int)n;
+//        System.out.println(num);
+//        System.out.println(n);
+        int a=5;
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+    }
+}
